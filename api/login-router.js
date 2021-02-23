dc2b794eda7d0ca57647f872b1c3e5eb18d16fb5
Ian-Tucker <iandecisiv@gmail.com>
2021-02-22 20:25:34 -0600

    working on routes

--- api/login-router.js
+++ api/login-router.js
@@ -6,5 +6,36 @@ const Users = require('./usersModel.js');
 const secrets = require('./secrets.js');
 //TODO: FINISH LOGIN ROUTER, DATA MIGRATIONS FOR USERS
 router.post("/", (req, res) => {
-    let { username,}
-})
+    let { username, password } = req.body;
+    Users.findBy({ username })
+        .then(([user]) => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = generateToken(user);
+                res.status(200).json({ 
+                    message: "Welcome!", token
+                });
+            } else {
+                res.status(401).json({ 
+                    message: "Can not access"
+                });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ 
+                errorMessage: error.message
+            });
+        });
+});
+
+function generateToken(user) {
+    const payload = {
+        userId: user.id,
+        username: user.username,
+    };
+    const secret = secret.jwtSecret;
+    const options = {
+        expiresIn: "5d",
+    };
+    return jwt.sign(payload, secret, options);
+}
